111d6dee7b254c0ef2754f339617260b0ed1e42f
hunter95001 <hunter95001@naver.com>
2019-02-18 17:03:31 +0900

    데이터 분석용pictureBox 3개 chart 2개

---
+++ DataAnalysis/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DataAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.캡처ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 293);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.캡처ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1477, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 캡처ToolStripMenuItem
+            // 
+            this.캡처ToolStripMenuItem.Name = "캡처ToolStripMenuItem";
+            this.캡처ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.캡처ToolStripMenuItem.Text = "캡처";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(517, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(440, 293);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1013, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(440, 293);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(31, 355);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(653, 312);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(776, 355);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(653, 312);
+            this.chart2.TabIndex = 10;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 695);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 캡처ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ DataAnalysis/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAnalysis
+{
+
+    public partial class Form1 : Form
+    {
+        const int imagenum = 1;
+
+        Bitmap bitmap1;
+        Bitmap bitmap2;
+        Bitmap bitmap3;
+
+        String imagePath1 = "C:\\Users\\KYJ\\Desktop\\Git Hub\\Som Cluster\\som cluster\\som cluster\\Image\\의사사진\\" + imagenum + "doctor.jpg";
+        String imagePath2 = "C:\\Users\\KYJ\\Desktop\\Git Hub\\Som Cluster\\som cluster\\som cluster\\Image\\어깨사진\\" + imagenum + "origin.jpg";
+        string savefilename = "C:\\Users\\KYJ\\Desktop\\룩업\\" + imagenum + ".jpg";
+
+        
+        public Form1()
+        {
+            //Part #1 초기화 부분 
+            InitializeComponent();
+            Init();
+
+            DataProcessing();
+
+            pictureBox1.Image = bitmap1;
+            pictureBox2.Image = bitmap2;
+            pictureBox3.Image = bitmap3;
+            chart1lookup(bitmap2);
+        }
+        public void Init() {
+             bitmap1 = new Bitmap(imagePath1);
+             bitmap2 = new Bitmap(imagePath2);
+             bitmap3 = new Bitmap(bitmap1.Width, bitmap1.Height);
+        }
+
+        public void DataProcessing()
+        {
+            
+            for (int x = 0; x < bitmap1.Width; x++)
+                for (int y = 0; y < bitmap1.Height; y++)
+                {
+                    bitmap3.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+
+            for (int x = 0; x < bitmap1.Width; x++)
+                for (int y = 0; y < bitmap1.Height; y++)
+                {
+                    int R = bitmap1.GetPixel(x, y).R;
+                    int G = bitmap1.GetPixel(x, y).G;
+                    int B = bitmap1.GetPixel(x, y).B;
+                    int R1 = bitmap2.GetPixel(x, y).R;
+                    int G1 = bitmap2.GetPixel(x, y).G;
+                    int B1 = bitmap2.GetPixel(x, y).B;
+                    if (R > G + B && R1 < 240)
+                    {
+                        bitmap3.SetPixel(x, y, Color.FromArgb(R1, 0, 0));
+                    }
+                }
+        }
+        
+        public void chart1lookup(Bitmap origanlBitmap)
+        {
+            int[] array = new int[256];
+            for (int x = 0; x < origanlBitmap.Width; x++)
+                for (int y = 0; y < origanlBitmap.Height; y++)
+                {
+                    int color = origanlBitmap.GetPixel(x, y).R;
+                    array[color]++;
+
+                }
+
+
+            chart1.Titles.Add("Lookup Table");
+            chart1.Series.Add("Color");
+            for (int i = 0; i < 255; i++)
+                chart1.Series["Color"].Points.AddXY(i.ToString(), array[i]);
+        }
+
+        public void chart2lookup(Bitmap origanlBitmap)
+        {
+            int[] array = new int[256];
+            for (int x = 0; x < origanlBitmap.Width; x++)
+                for (int y = 0; y < origanlBitmap.Height; y++)
+                {
+                    int color = origanlBitmap.GetPixel(x, y).R;
+                    array[color]++;
+
+                }
+
+
+            chart2.Titles.Add("Lookup Table");
+            chart2.Series.Add("Color");
+            for (int i = 0; i < 255; i++)
+                chart2.Series["Color"].Points.AddXY(i.ToString(), array[i]);
+        }
+
+        private void 캡처ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(0, 0, ClientSize.Width, ClientSize.Height);
+            Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(this.Left, this.Top, 0, 0, this.Size);
+            bitmap.Save(savefilename);
+        }
+
+    }
+}
